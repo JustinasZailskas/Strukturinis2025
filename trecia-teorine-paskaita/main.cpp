0f4f53e66ee3af6be6510b84d001604211a3709d
JustinasZailskas <justinaszailskas@gmail.com>
2025-09-23 11:26:42 +0300

    for ciklo pavyzdys

--- trecia-teorine-paskaita/main.cpp
+++ trecia-teorine-paskaita/main.cpp
@@ -39,37 +39,49 @@ int main() {
     //     default:
     //         cout <<"Tokios savaites dienos nera"<< endl;
     // }
-    int diena;
-    while(true) {
+    // int diena;
+    // while(true) {
+    //
+    //     cout <<"Iveskite diena (1-7)"<<endl;
+    //     cin>>diena;
+    //     switch (diena) {
+    //         case 1:
+    //             cout << "Pirmadienis"<< endl;
+    //         break;
+    //         case 2:
+    //             cout <<"Antradienis"<< endl;
+    //         break;
+    //         case 3:
+    //             cout << "Treciadienis"<< endl;
+    //         break;
+    //         case 4:
+    //             cout <<"ketvirtadienis"<< endl;
+    //         break;
+    //         case 5:
+    //             cout << "Penktadienis"<< endl;
+    //         break;
+    //         case 6:
+    //             cout <<"Sestadienis"<< endl;
+    //         break;
+    //         case 7:
+    //             cout <<"Sekmadienis"<<endl;
+    //         break;
+    //         default:
+    //             cout <<"Tokios savaites dienos nera"<< endl;
+    //     }
+    //
+    // }
 
-        cout <<"Iveskite diena (1-7)"<<endl;
-        cin>>diena;
-        switch (diena) {
-            case 1:
-                cout << "Pirmadienis"<< endl;
-            break;
-            case 2:
-                cout <<"Antradienis"<< endl;
-            break;
-            case 3:
-                cout << "Treciadienis"<< endl;
-            break;
-            case 4:
-                cout <<"ketvirtadienis"<< endl;
-            break;
-            case 5:
-                cout << "Penktadienis"<< endl;
-            break;
-            case 6:
-                cout <<"Sestadienis"<< endl;
-            break;
-            case 7:
-                cout <<"Sekmadienis"<<endl;
-            break;
-            default:
-                cout <<"Tokios savaites dienos nera"<< endl;
-        }
-        
+    int number;
+    cout <<"Iveskite sveikaji skaiciu Number"<<endl;
+    cin >> number;
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++) {
+        sum = sum + i; // sum += i;
     }
+
+    cout <<"Suma skaiciu nuo 1 iki "<<number<<" yra: "<<sum<<endl;
+
     return 0;
 }
