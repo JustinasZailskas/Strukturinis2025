fdb60dedc6c0e75edb2d56c3caf0c7231443eb62
JustinasZailskas <justinaszailskas@gmail.com>
2025-09-30 11:32:21 +0300

    Rikiavimo pavyzdys

--- min-max-reiksmes-masyve/main.cpp
+++ min-max-reiksmes-masyve/main.cpp
@@ -3,7 +3,7 @@ using namespace std;// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> 
 int main() {
 
     int masyvas[100];
-    int n; //elementu kiekis masyve
+    int n, temp; //elementu kiekis masyve
     cout<<"Iveskite kiek elementu norite tureti masyve"<<endl;
     cin>>n;
     cout<<"Iveskite masyvo elementus"<<endl;
@@ -31,6 +31,21 @@ int main() {
     cout<<"Rezultatai"<<endl;
     cout<<"Didziausias masyvo elementas yra "<<max<<" jo pozicija "<<imax+1<<endl;
     cout<<"Maziausias masyvo elementas yra "<<min<<" jo pozicija "<<imin+1<<endl;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i +1; j < n; j++) {
+            if (masyvas[i] > masyvas[j]) {
+                temp = masyvas[i];
+                masyvas[i] = masyvas[j];
+                masyvas[j] = temp;
+            }
+        }
+    }
+    cout <<"Isrikiuoti elementai: "<<endl;
+    for (int i = 0; i < n; i++) {
+        cout<<masyvas[i]<<" ";
+    }
+
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
